1524cf010082ee9e5b2fc9c7efbe1d0eb11f3610
Mehdi <mehdiboulkertous@gmail.com>
2025-09-29 15:49:30 +0200

    test: correct EIP-712 digest (abi.encode + token.DOMAIN_SEPARATOR)

--- test/utils/SigUtils.sol
+++ test/utils/SigUtils.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-interface IERC20PermitLike {
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-}
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
 contract SigUtils {
+    // EXACTEMENT le même typehash que OZ
+    bytes32 internal constant PERMIT_TYPEHASH =
+        keccak256(
+            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+        );
+
+    IERC20Permit public immutable token;
+
+    constructor(IERC20Permit _token) {
+        token = _token;
+    }
+
     struct Permit {
         address owner;
         address spender;
@@ -14,34 +24,23 @@ contract SigUtils {
         uint256 deadline;
     }
 
-    bytes32 internal constant PERMIT_TYPEHASH =
-        keccak256(
-            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-        );
-
-    IERC20PermitLike public immutable token;
-
-    constructor(IERC20PermitLike _token) {
-        token = _token;
-    }
-
-    function getTypedDataHash(Permit memory p) public view returns (bytes32) {
-        bytes32 ds = token.DOMAIN_SEPARATOR();
-        return keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                ds,
-                keccak256(
-                    abi.encode(
-                        PERMIT_TYPEHASH,
-                        p.owner,
-                        p.spender,
-                        p.value,
-                        p.nonce,
-                        p.deadline
-                    )
-                )
+    function getTypedDataHash(Permit memory p) external view returns (bytes32) {
+        bytes32 structHash = keccak256(
+            abi.encode(
+                PERMIT_TYPEHASH,
+                p.owner,
+                p.spender,
+                p.value,
+                p.nonce,
+                p.deadline
             )
+        );
+        // Domaine lu sur le token (EIP712 d’OZ)
+        bytes32 domainSeparator = token.DOMAIN_SEPARATOR();
+
+        return keccak256(
+            abi.encodePacked("\x19\x01", domainSeparator, structHash)
         );
     }
 }
+
